47f87f03e30370b3779e563dba3db92207688626
Juan Diego Duque <juandiegoduquejimenez@gmail.com>
2025-05-02 15:53:19 -0500

    agregar item edad

--- src/main/java/com/example/labfaker/Persona.java
+++ src/main/java/com/example/labfaker/Persona.java
@@ -6,16 +6,18 @@ public class Persona {
     private String nombre;
     private String apellido;
     private String correo;
+    private int edad;
 
     public Persona() {
 
     }
 
-    public Persona(int id, String nombre, String apellido, String correo) {
+    public Persona(int id, String nombre, String apellido, String correo, int edad) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
+        this.edad = edad;
     }
 
     public int getId() {
@@ -48,5 +50,13 @@ public class Persona {
 
     public void setCorreo(String correo) {
         this.correo = correo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 }
